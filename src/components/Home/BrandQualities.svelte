ae1e13987b7392a37bc860c01975ab6bc66151af
Marcin J <jerzakm@gmail.com>
2021-02-08 11:09:30 +0100

    brand qualities responsive

--- src/components/Home/BrandQualities.svelte
+++ src/components/Home/BrandQualities.svelte
@@ -1,7 +1,15 @@
-<section class="text-gray-600 body-font px-24 py-8 overflow-hidden">
-  <div class="container flex">
+<style>
+  @media(max-width: 1240px) and (min-width: 1024px) {
+    .hideminmax{
+      display: none;
+    }
+  }
+</style>
 
-    <div class="flex items-center lg:w-3/5 pb-10 mb-10  sm:flex-row flex-col">
+<section class="text-gray-600 body-font px-24 py-0 overflow-hidden ">
+  <div class="container flex flex-col lg:flex-row items-center justify-center">
+
+    <div class="flex items-center lg:w-3/5 pb-6 mb-6  sm:flex-row flex-col">
       <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full  text-black flex-shrink-0">
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
           <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
@@ -9,12 +17,11 @@
       </div>
       <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
         <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Jakość</h2>
-        <p class="leading-relaxed text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non eveniet ratione voluptas doloribus. Reiciendis nisi ratione repellat quod!</p>
-
+        <p class="leading-relaxed text-base hideminmax">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non eveniet ratione voluptas doloribus. Reiciendis nisi ratione repellat quod!</p>
       </div>
     </div>
 
-    <div class="flex items-center lg:w-3/5 pb-10 mb-10  sm:flex-row flex-col">
+    <div class="flex items-center lg:w-3/5 pb-6 mb-6  sm:flex-row flex-col">
       <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full  text-black flex-shrink-0">
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
           <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
@@ -22,12 +29,12 @@
       </div>
       <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
         <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Wieloletnie doświadczenie</h2>
-        <p class="leading-relaxed text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non eveniet ratione voluptas doloribus. Reiciendis nisi ratione repellat quod!</p>
+        <p class="leading-relaxed text-base hideminmax">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non eveniet ratione voluptas doloribus. Reiciendis nisi ratione repellat quod!</p>
 
       </div>
     </div>
 
-    <div class="flex items-center lg:w-3/5 pb-10 mb-10  sm:flex-row flex-col">
+    <div class="flex items-center lg:w-3/5 pb-6 mb-6  sm:flex-row flex-col">
       <div class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full text-black flex-shrink-0">
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="sm:w-16 sm:h-16 w-10 h-10" viewBox="0 0 24 24">
           <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
@@ -35,7 +42,7 @@
       </div>
       <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
         <h2 class="text-gray-900 text-lg title-font font-medium mb-2">Wsparcie i obsługa</h2>
-        <p class="leading-relaxed text-base">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non eveniet ratione voluptas doloribus. Reiciendis nisi ratione repellat quod!</p>
+        <p class="leading-relaxed text-base hideminmax">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non eveniet ratione voluptas doloribus. Reiciendis nisi ratione repellat quod!</p>
 
       </div>
     </div>
