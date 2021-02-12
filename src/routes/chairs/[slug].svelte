65b32f2909ca2118c0e841121f6650b60827d92b
Marcin J <jerzakm@gmail.com>
2021-02-12 10:12:41 +0100

    product page data integration

--- src/routes/chairs/[slug].svelte
+++ src/routes/chairs/[slug].svelte
@@ -1,5 +1,29 @@
+<script context="module">
+  import YAML from 'YAML'
+
+	export async function preload(page) {
+
+    const chairModel = page.params.slug
+
+    const response = await this.fetch(`content/chairs/${chairModel}.yaml`);
+    const yaml = await response.text()
+    const data = YAML.parse(yaml)
+
+    return {data}
+	}
+</script>
+
 <script>
   import TransitionWrapper from '../../components/animations/TransitionWrapper.svelte'
+  import { colorDictionary } from '../../constants/colorDictionary'
+  import { specsDictionary } from '../../constants/specsDictionary'
+
+  export let data
+
+  let variantChosen = 0
+
+
+
 </script>
 <TransitionWrapper>
   <div class="px-6 md:px-12 xxl:px-32">
@@ -7,69 +31,41 @@
           <div class="container py-24 pb-6 ">
             <div class="mx-auto flex flex-wrap">
               <div class="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center ">
-                  <img alt="ecommerce" class="w-3/4 mx-auto shadow-2xl" src="skadi\Skadi_fotel_biurowy_ergonomiczny_materiałowy_nordhold_czarny_1.JPG">
+                  <img alt="ecommerce" class="w-3/4 mx-auto shadow-2xl" src={`img/${data.slug}/${data.variants[variantChosen].images[0]}.jpg`}>
               </div>
               <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                <h2 class="text-md title-font text-gray-500 tracking-widest">Seria Ergo</h2>
-                <h1 class="text-gray-900 text-5xl title-font font-medium mb-1" style="font-weight: 700;">Skadi</h1>
+                <h2 class="text-md title-font text-gray-500 tracking-widest">{data.series.pl}</h2>
+                <h1 class="text-gray-900 text-5xl title-font font-medium mb-1" style="font-weight: 700;">{data.name}</h1>
                 <div class="flex mb-4">
                   <span class="flex items-center">
-                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                    </svg>
-                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                    </svg>
-                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                    </svg>
-                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                    </svg>
-                    <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
-                      <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-                    </svg>
-                    <span class="text-gray-600 ml-3">4 Oceny</span>
-                  </span>
-                  <span class="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-                    <a class="text-gray-500" href=".">
-                      <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                        <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                    {#each {length: data.rating} as g,i}
+                      <svg fill="currentColor" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 text-yellow-500" viewBox="0 0 24 24">
+                        <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
                       </svg>
-                    </a>
-                    <a class="text-gray-500" href=".">
-                      <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                        <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-                      </svg>
-                    </a>
+                    {/each}
+                    <span class="text-gray-600 ml-3">({data.testimonials.length})</span>
                   </span>
                 </div>
-                <p class="leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut mollitia sequi distinctio aperiam eligendi minima accusantium in neque numquam perspiciatis ipsum, quo magni accusamus aliquam recusandae, dolorum itaque possimus praesentium!</p>
+                <p class="leading-relaxed">{data.description.pl}</p>
                 <div class="flex mt-6 items-center pb-5 border-b-2 border-gray-400 mb-5">
                   <div class="flex">
                     <span class="mr-3">Kolor</span>
-                    <button class="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                    <button class="border-2 border-gray-300 ml-1 bg-gray-900 rounded-full w-6 h-6 focus:outline-none"></button>
-                    <button class="border-2 border-gray-300 ml-1 bg-red-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                    <button class="border-2 border-gray-300 ml-1 bg-gray-600 rounded-full w-6 h-6 focus:outline-none"></button>
+                    {#each data.variants as variant, variantIndex}
+                      <button class={`border-2 border-gray-300 ml-1 bg-${colorDictionary[variant.color]} rounded-full w-6 h-6 focus:outline-none`}
+                        on:click={()=> variantChosen = variantIndex}
+                      />
+                    {/each}
                   </div>
 
                 </div>
                 <div class="flex">
-                  <span class="title-font font-medium text-2xl text-gray-900">499.00 zł</span>
-                  <!-- <button class="flex ml-auto text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Button</button> -->
-                  <!-- <button class="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
-                    <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-                      <path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
-                    </svg>
-                  </button> -->
+                  <span class="title-font font-medium text-2xl text-gray-900">{data.price} zł</span>
                 </div>
 
-                <div class="grid grid-cols-3 py-8 text-gray-900">
-                  {#each {length: 6} as g,i}
-                      <span class="text-gray-700 pb-1 tracking-wide col-span-1">Szerokość siedziska:</span>
-                      <span class="text-gray-800 pb-1 col-span-2">50 cm</span>
-
+                <div class="grid grid-cols-2 py-8 text-gray-900">
+                  {#each Object.keys(data.specs) as specKey,i}
+                      <span class="text-gray-700 pb-1 tracking-wide">{specsDictionary[specKey].pl}:</span>
+                      <span class="text-gray-800 pb-1">{data.specs[specKey]}</span>
                   {/each}
                 </div>
               </div>
@@ -86,31 +82,28 @@
             <div class="flex flex-wrap md:-m-2 -m-1">
               <div class="flex flex-wrap w-1/2">
                 <div class="md:p-2 p-1 w-1/2 ">
-                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl block" src="skadi\Skadi_fotel_biurowy_ergonomiczny_materiałowy_nordhold_czarny_3.JPG">
+                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl block" src={`img/${data.slug}/${data.variants[variantChosen].images[6]}.jpg`}>
                 </div>
                 <div class="md:p-2 p-1 w-1/2 ">
-                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl  block" src="skadi\Skadi_fotel_biurowy_ergonomiczny_materiałowy_nordhold_pochylanie.JPG">
+                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl  block" src={`img/${data.slug}/${data.variants[variantChosen].images[3]}.jpg`}>
                 </div>
                 <div class="md:p-2 p-1 w-full ">
-                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl  block" src="skadi\Skadi_fotel_biurowy_ergonomiczny_materiałowy_nordhold_czarny_2.jpg">
+                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl  block" src={`img/${data.slug}/${data.variants[variantChosen].images[5]}.jpg`}>
                 </div>
               </div>
               <div class="flex flex-wrap w-1/2">
                 <div class="md:p-2 p-1 w-full">
-                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl  block" src="skadi\Skadi_fotel_biurowy_ergonomiczny_materiałowy_nordhold_czarny_4.jpg">
+                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl  block" src={`img/${data.slug}/${data.variants[variantChosen].images[1]}.jpg`}>
                 </div>
                 <div class="md:p-2 p-1 w-1/2 ">
-                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl  block" src="skadi\Skadi_fotel_biurowy_ergonomiczny_materiałowy_nordhold_podłokietnik.jpg">
+                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl  block" src={`img/${data.slug}/${data.variants[variantChosen].images[4]}.jpg`}>
                 </div>
                 <div class="md:p-2 p-1 w-1/2 ">
-                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl  block" src="skadi\Skadi_fotel_biurowy_ergonomiczny_materiałowy_nordhold_podłokietnik4.jpg">
+                  <img alt="gallery" class="object-cover w-full h-full shadow-2xl  block" src={`img/${data.slug}/${data.variants[variantChosen].images[2]}.jpg`}>
                 </div>
               </div>
             </div>
           </div>
         </section>
-
-
-
   </div>
 </TransitionWrapper>
